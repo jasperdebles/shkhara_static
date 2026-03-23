90ab880924fa017d9273a6d3dfdd7aa7bf7f6585
jdbles <jasper.debles@ugent.be>
2026-03-23 12:26:57 +0100

    edited contact form with hidden input

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -6,6 +6,7 @@
         <h1 class="text-white text-4xl font-bold text-center mb-8">Contact</h1>
 
         <form name="contact" method="POST" data-netlify="true" class="flex flex-col space-y-4">
+            <input type="hidden", name="form-name" value="contact">
             <input name="name" placeholder="Name" required class="w-full p-4 rounded-xl border border-white/30 text-white bg-white/10 placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-sky-400">
             <input name="_replyto" type="email" placeholder="E-mail" required class="w-full p-4 rounded-xl border border-white/30 text-white bg-white/10 placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-sky-400">
             <textarea name="message" placeholder="Tell us about your challenges!" class="w-full p-4 rounded-xl border border-white/30 bg-white/10 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-sky-400"></textarea>
