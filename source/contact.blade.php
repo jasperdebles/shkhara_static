ad4d30390be7aef0160568d81b27594bb4c939b3
jdbles <jasper.debles@ugent.be>
2026-03-23 12:31:55 +0100

    test contact form

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -9,7 +9,7 @@
             <input type="hidden", name="form-name" value="contact">
             <input name="name" placeholder="Name" required class="w-full p-4 rounded-xl border border-white/30 text-white bg-white/10 placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-sky-400">
             <input name="_replyto" type="email" placeholder="E-mail" required class="w-full p-4 rounded-xl border border-white/30 text-white bg-white/10 placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-sky-400">
-            <textarea name="message" placeholder="Tell us about your challenges!" class="w-full p-4 rounded-xl border border-white/30 bg-white/10 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-sky-400"></textarea>
+            <textarea name="message" placeholder="Tell us about your current challenges!" class="w-full p-4 rounded-xl border border-white/30 bg-white/10 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-sky-400"></textarea>
             <button 
                 type="submit" class="w-full bg-sky-400 hover:bg-sky-300 text-slate-900 py-4 px-8 rounded-xl font-bold text-lg shadow-lg hover:shadow-xl transition-all">Submit
             </button>
